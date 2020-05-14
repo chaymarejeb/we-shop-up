e49faa966ffe88fafd11789c5da436d38a2a3fad
chinamiee <38914459+chinamiee@users.noreply.github.com>
2020-05-14 14:17:21 +0200

    configuration button login

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -25,10 +25,11 @@ export class LoginComponent implements OnInit {
   //this._auth.loginUser(this.loginUserData)
 
   loginUser (f) {
-
+     console.log ('form')
     this._auth.loginUser(this.loginUserData)
     .subscribe(
       res => {
+        console.log ('res',res)
         localStorage.setItem('token', res.token)
         this._router.navigate(['/special-events'])
       },
